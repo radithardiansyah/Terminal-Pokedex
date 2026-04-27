0b7d842c36a7c1d205cfcd7180a5ad52c7080ef7
radit <rdithardiansyah@gmail.com>
2026-04-27 21:49:02 +0700

    finish

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# Terminal-Pokodex
-here's a fun project i build in order to learning typescript
+# Terminal- Pokédex
+
+here's a fun project i build to learn typescript
+
+the pokedex has the following feautures:
+
+#1 View Pokémon stats
+-Accessible by typing a Pokémon's name
+
+#2 Add a Pokémon to your favorite list
+-can be access by clicking enter on 'add' option, bellow stats section
+
+#3 Remove pokemon from your favorite list
+- comming soon (hopefully)
 
 To install dependencies:
 
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as p from "@clack/prompts";
+
 interface PokeStats {
   name: string;
   height: number;
@@ -11,9 +13,12 @@ async function fetchR<T>(url: string): Promise<T> {
   return res.json() as T;
 }
 
+const pokemonFav: string[] = [];
 async function PokeSearch(PokemonName: string) {
+  PokemonName = PokemonName.toLowerCase();
   let url: string = `https://pokeapi.co/api/v2/pokemon/${PokemonName}`;
   const data = await fetchR<PokeStats>(url);
+  console.log("\n========== POKEDEX ==========");
   console.log(`Name: ${data.name}`);
   console.log(`Height: ${data.height}`);
   console.log(`Weight: ${data.weight}`);
@@ -21,21 +26,50 @@ async function PokeSearch(PokemonName: string) {
   console.log(`Types: ${types.join(", ")}`);
   const abilities = data.abilities.map((a: any) => a.ability.name);
   console.log(`Abilities: ${abilities.join(", ")}`);
+  console.log("================================\n");
+
+  const action = await p.select({
+    message: `add ${PokemonName} to your favorite list?`,
+    options: [
+      { value: "favorite", label: "add" },
+      { value: "no", label: "no" },
+    ],
+  });
+
+  if (action === "favorite") {
+    pokemonFav.push(`${PokemonName}`);
+    console.log(`${PokemonName} added to favorites!`);
+  }
 }
 
 let running: boolean = true;
 async function main() {
   while (running === true) {
-    console.log(
-      "Wich Pokemon? || Type 'fav' To See Favorite List || Type 'exit' To Stop ",
-    );
-    const input = prompt("-");
+    const input = await p.text({
+      message: "Which Pokemon? ('fav' / 'exit')",
+    });
+
+    if (p.isCancel(input)) {
+      process.exit(0);
+    }
+
     if (input === "exit" || input === null) {
       break;
     } else if (input === "fav") {
-    } else {
-      await PokeSearch(input);
+      if (pokemonFav.length === 0) {
+        console.log("\n========== FAVORITES ==========");
+        console.log("you dont have favorite pokemon yet");
+        console.log("================================\n");
+      } else {
+        pokemonFav.forEach((name, index) => {
+          console.log("\n========== FAVORITES ==========");
+          console.log(`${index + 1}. ${name}`);
+          console.log("================================\n");
+        });
+      }
+      continue;
     }
+    await PokeSearch(input);
   }
 }
 
